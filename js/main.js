f30c10b537412896534302b63ee50f7c74498350
Chad Stever <castever@gmail.com>
2016-05-26 11:57:27 -0400

    use let; object destructuring

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-var calculateMonthlyPayment = function (principal, years, rate) {
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
-    }
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+let calculateMonthlyPayment = function(principal, years, rate) {
+  let monthlyRate = 0;
+  if (rate) {
+    monthlyRate = rate / 100 / 12;
+  }
+  let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+  return {
+    principal,
+    years,
+    rate,
+    monthlyPayment,
+    monthlyRate
+  };
 };
 
-document.getElementById('calcBtn').addEventListener('click', function () {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+document.getElementById('calcBtn').addEventListener('click', function() {
+  let principal = document.getElementById("principal").value;
+  let years = document.getElementById("years").value;
+  let rate = document.getElementById("rate").value;
+  let {
+    monthlyPayment,
+    monthlyRate
+  } = calculateMonthlyPayment(principal, years, rate);
+  document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+  document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
